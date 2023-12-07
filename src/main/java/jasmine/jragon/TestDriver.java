e7f174a281fd1d5c35e2ae5a720c06e9f1e5e698
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2023-12-06 23:51:37 -0600

    Added a TestDriver

---
+++ src/main/java/jasmine/jragon/TestDriver.java
@@ -0,0 +1,23 @@
+package jasmine.jragon;
+
+import jasmine.jragon.stream.collector.restream.grouping.KeyedGroup;
+import jasmine.jragon.tuple.Tuple;
+import jasmine.jragon.tuple.type.Duo;
+
+import java.util.stream.Collectors;
+
+public class TestDriver {
+    public static void main(String[] args) {
+        Tuple t1 = Tuple.of(123, 89.0, 56.6f, 1239829348723492310L);
+        Tuple t2 = Tuple.of(123, 67, 45.0, 'c');
+        System.out.println(
+                t1.union(t2)
+                        .enumerate()
+                        .collect(KeyedGroup.provide(e -> e.getIndex() % 3))
+                        .peek(e -> System.out.println(e.getFirst()))
+                        .map(Duo::getSecond)
+                        .map(String::valueOf)
+                        .collect(Collectors.joining("\n"))
+        );
+    }
+}
