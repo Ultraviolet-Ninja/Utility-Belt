6b06fd9d74cafbff4321ca40e81357002b11b95f
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2023-12-07 00:02:26 -0600

    Added interface that deals with different sets of Collector characteristics

---
+++ src/main/java/jasmine/jragon/stream/collector/CustomCollector.java
@@ -0,0 +1,18 @@
+package jasmine.jragon.stream.collector;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.stream.Collector;
+
+import static java.util.Collections.unmodifiableSet;
+import static java.util.stream.Collector.Characteristics.CONCURRENT;
+import static java.util.stream.Collector.Characteristics.IDENTITY_FINISH;
+import static java.util.stream.Collector.Characteristics.UNORDERED;
+
+public interface CustomCollector<T, A, R> extends Collector<T, A, R> {
+    Set<Collector.Characteristics> NONE = Collections.emptySet();
+    Set<Collector.Characteristics> IDENTITY = unmodifiableSet(EnumSet.of(IDENTITY_FINISH));
+    Set<Collector.Characteristics> CONCURRENT_IDENTITY = unmodifiableSet(EnumSet.of(CONCURRENT, IDENTITY_FINISH));
+    Set<Collector.Characteristics> UNORDERED_IDENTITY = unmodifiableSet(EnumSet.of(UNORDERED, IDENTITY_FINISH));
+}
