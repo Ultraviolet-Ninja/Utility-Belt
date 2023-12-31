b32a1ce11128142d3ac3ab1925d4d83c7875287c
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2023-12-31 17:26:25 -0600

    Simplifying the process of going from iterators to streams

---
+++ src/main/java/jasmine/jragon/stream/support/SteamAssistant.java
@@ -0,0 +1,35 @@
+package jasmine.jragon.stream.support;
+
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.util.Spliterator.ORDERED;
+import static java.util.Spliterator.SIZED;
+
+public final class SteamAssistant {
+    public static final int ORDERED_SIZED = SIZED | ORDERED;
+
+    public static final int SORTED = Spliterator.SORTED | ORDERED;
+
+    public static <T> Stream<T> fromIterator(Iterable<T> typedIterable, int characteristics) {
+        var spliterator = Spliterators.spliteratorUnknownSize(typedIterable.iterator(), characteristics);
+        return StreamSupport.stream(spliterator, false);
+    }
+
+    public static <T> Stream<T> fromIterator(Iterable<T> typedIterable, int size, int characteristics) {
+        var spliterator = Spliterators.spliterator(typedIterable.iterator(), size, characteristics | SIZED);
+        return StreamSupport.stream(spliterator, false);
+    }
+
+    public static <T> Stream<T> fromIteratorParallel(Iterable<T> typedIterable, int characteristics) {
+        var spliterator = Spliterators.spliteratorUnknownSize(typedIterable.iterator(), characteristics);
+        return StreamSupport.stream(spliterator, true);
+    }
+
+    public static <T> Stream<T> fromIteratorParallel(Iterable<T> typedIterable, int size, int characteristics) {
+        var spliterator = Spliterators.spliterator(typedIterable.iterator(), size, characteristics | SIZED);
+        return StreamSupport.stream(spliterator, true);
+    }
+}
