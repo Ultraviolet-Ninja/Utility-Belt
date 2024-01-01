1b06c5d29f9c2c2b1c6828eeb5dba3fba57627fa
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2024-01-01 12:39:20 -0600

    Converting to records

--- src/main/java/jasmine/jragon/enumerated/EnumeratedElement.java
+++ src/main/java/jasmine/jragon/enumerated/EnumeratedElement.java
@@ -1,22 +1,12 @@
 package jasmine.jragon.enumerated;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Comparator;
 
-@Getter
-@RequiredArgsConstructor
-@EqualsAndHashCode(doNotUseGetters = true, cacheStrategy = EqualsAndHashCode.CacheStrategy.LAZY)
-public final class EnumeratedElement<T> implements Comparable<EnumeratedElement<T>> {
-    private final int index;
-    @NonNull
-    private final T element;
-
+public record EnumeratedElement<T>(int index, @NonNull T element) implements Comparable<EnumeratedElement<T>> {
     @Override
     public String toString() {
         return String.format("{Index: %d - Element: %s}", index, element);
