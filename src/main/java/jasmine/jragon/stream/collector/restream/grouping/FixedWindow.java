652790a9cecba318e14e32e7ef441b8c8d1dd174
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2024-01-01 13:26:21 -0600

    Reflected changes from v17

--- src/main/java/jasmine/jragon/stream/collector/restream/grouping/FixedWindow.java
+++ src/main/java/jasmine/jragon/stream/collector/restream/grouping/FixedWindow.java
@@ -1,5 +1,6 @@
 package jasmine.jragon.stream.collector.restream.grouping;
 
+import jasmine.jragon.stream.collector.restream.StreamCheckpoint;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.Contract;
@@ -20,8 +21,8 @@ import java.util.stream.Stream;
  * @param <C> The collection used to store the incoming type
  */
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-public final class FixedWindow<T, C extends Collection<T>> implements Window<T, C> {
-    private final int groupSize;
+public final class FixedWindow<T, C extends Collection<T>> implements StreamCheckpoint<T, C> {
+    private final int windowSize;
     private final Supplier<C> collectionSupplier;
 
     private C currentCollection;
@@ -30,7 +31,7 @@ public final class FixedWindow<T, C extends Collection<T>> implements Window<T, 
     @Override
     public @NotNull BiConsumer<Stream.Builder<C>, T> accumulator() {
         return (cBuilder, type) -> {
-            if (currentCollection == null || currentCollection.size() % groupSize == 0) {
+            if (currentCollection == null || currentCollection.size() % windowSize == 0) {
                 currentCollection = collectionSupplier.get();
                 cBuilder.add(currentCollection);
             }
@@ -40,25 +41,35 @@ public final class FixedWindow<T, C extends Collection<T>> implements Window<T, 
     }
 
     @Contract("_ -> new")
-    public static <T> @NotNull FixedWindow<T, List<T>> group(int groupSize) {
-        return new FixedWindow<>(groupSize, ArrayList::new);
+    public static <T> @NotNull FixedWindow<T, List<T>> group(int windowSize) {
+        validateWindowSize(windowSize);
+        return new FixedWindow<>(windowSize, ArrayList::new);
     }
 
     @Contract("_, _ -> new")
-    public static <T, C extends List<T>> @NotNull FixedWindow<T, C> groupToLists(int groupSize,
+    public static <T, C extends List<T>> @NotNull FixedWindow<T, C> groupToLists(int windowSize,
                                                                                  Supplier<C> listSupplier) {
-        return new FixedWindow<>(groupSize, listSupplier);
+        validateWindowSize(windowSize);
+        return new FixedWindow<>(windowSize, listSupplier);
     }
 
     @Contract("_, _ -> new")
-    public static <T, C extends Set<T>> @NotNull FixedWindow<T, C> groupToSets(int groupSize,
+    public static <T, C extends Set<T>> @NotNull FixedWindow<T, C> groupToSets(int windowSize,
                                                                                Supplier<C> setSupplier) {
-        return new FixedWindow<>(groupSize, setSupplier);
+        validateWindowSize(windowSize);
+        return new FixedWindow<>(windowSize, setSupplier);
     }
 
     @Contract("_, _ -> new")
-    public static <T, C extends Collection<T>> @NotNull FixedWindow<T, C> groupToSpecified(int groupSize,
+    public static <T, C extends Collection<T>> @NotNull FixedWindow<T, C> groupToSpecified(int windowSize,
                                                                                            Supplier<C> collectionSupplier) {
-        return new FixedWindow<>(groupSize, collectionSupplier);
+        validateWindowSize(windowSize);
+        return new FixedWindow<>(windowSize, collectionSupplier);
+    }
+
+    private static void validateWindowSize(int windowSize) {
+        if (windowSize < 2) {
+            throw new IllegalArgumentException("Window size must be at least 2");
+        }
     }
 }
