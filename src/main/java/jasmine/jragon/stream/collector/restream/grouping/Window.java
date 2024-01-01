c17f73a0674af374ce53e676022cebb11322cc40
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2024-01-01 12:40:55 -0600

    Added a general Window interface

--- src/main/java/jasmine/jragon/stream/collector/restream/grouping/Window.java
+++ src/main/java/jasmine/jragon/stream/collector/restream/grouping/Window.java
@@ -4,23 +4,22 @@ import jasmine.jragon.stream.collector.CustomCollector;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
 import java.util.Set;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
-public interface Window<T, C extends Collection<T>> extends CustomCollector<T, Stream.Builder<C>, Stream<C>> {
+public interface Window<T, R> extends CustomCollector<T, Stream.Builder<R>, Stream<R>> {
     @Contract(pure = true)
     @Override
-    default @NotNull Supplier<Stream.Builder<C>> supplier() {
+    default @NotNull Supplier<Stream.Builder<R>> supplier() {
         return Stream::builder;
     }
 
     @Contract(pure = true)
     @Override
-    default @NotNull BinaryOperator<Stream.Builder<C>> combiner() {
+    default @NotNull BinaryOperator<Stream.Builder<R>> combiner() {
         return (left, right) -> {
             right.build().forEach(left::add);
             return left;
@@ -29,7 +28,7 @@ public interface Window<T, C extends Collection<T>> extends CustomCollector<T, S
 
     @Contract(pure = true)
     @Override
-    default @NotNull Function<Stream.Builder<C>, Stream<C>> finisher() {
+    default @NotNull Function<Stream.Builder<R>, Stream<R>> finisher() {
         return Stream.Builder::build;
     }
 
