91e2e99ce6e4a33c43a7e78e1ae3ab86cb75d263
Mr. J <mjohnsonsdev@outlook.com>
2025-04-02 14:37:20 -0500

    Added consumer methods to the types tuples

--- src/main/java/jasmine/jragon/tuple/type/Duo.java
+++ src/main/java/jasmine/jragon/tuple/type/Duo.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -34,6 +37,26 @@ public final class Duo<A, B> {
         );
     }
 
+    @Contract(value = " -> new", pure = true)
+    public Map.@NotNull Entry<A, B> toEntry() {
+        return new AbstractMap.SimpleEntry<>(first, second);
+    }
+
+    @Contract(value = " -> new", pure = true)
+    public Map.@NotNull Entry<A, B> toImmutableEntry() {
+        return new AbstractMap.SimpleImmutableEntry<>(first, second);
+    }
+
+    @Contract("_ -> new")
+    public <C> @NotNull Trio<C, A, B> addFirst(C newFirst) {
+        return new Trio<>(newFirst, this.first, this.second);
+    }
+
+    @Contract("_ -> new")
+    public <C> @NotNull Trio<A, C, B> addSecond(C newSecond) {
+        return new Trio<>(this.first, newSecond, this.second);
+    }
+
     @Contract("_ -> new")
     public <C> @NotNull Trio<C, A, B> addToFirst(C head) {
         return new Trio<>(head, this.first, this.second);
@@ -53,6 +76,10 @@ public final class Duo<A, B> {
         return reducer.apply(first, second);
     }
 
+    public void sendTo(@NotNull BiConsumer<? super A, ? super B> action) {
+        action.accept(first, second);
+    }
+
     @Override
     public String toString() {
         return String.format("First Element %s%nSecond Element %s%n", first, second);
@@ -61,5 +88,10 @@ public final class Duo<A, B> {
     @Contract("_, _ -> new")
     public static <T, U> @NotNull Duo<T, U> of(T first, U second) {
         return new Duo<>(first, second);
+    }
+
+    @Contract("_ -> new")
+    public static <T, U> @NotNull Duo<T, U> fromEntry(Map.@NotNull Entry<T, U> entry) {
+        return new Duo<>(entry.getKey(), entry.getValue());
     }
 }
---
+++ src/main/java/jasmine/jragon/tuple/type/Quad.java
@@ -0,0 +1,87 @@
+package jasmine.jragon.tuple.type;
+
+import jasmine.jragon.function.QuadConsumer;
+import jasmine.jragon.function.QuadFunction;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
+
+@Getter
+@RequiredArgsConstructor
+@EqualsAndHashCode(doNotUseGetters = true, cacheStrategy = EqualsAndHashCode.CacheStrategy.LAZY)
+public final class Quad<A, B, C, D> {
+    private final A first;
+    private final B second;
+    private final C third;
+    private final D fourth;
+
+    @Contract("_ -> new")
+    public <E> @NotNull Quad<E, B, C, D> mapFirst(@NotNull Function<A, E> mapper) {
+        return new Quad<>(mapper.apply(first), second, third, fourth);
+    }
+
+    @Contract("_ -> new")
+    public <E> @NotNull Quad<A, E, C, D> mapSecond(@NotNull Function<B, E> mapper) {
+        return new Quad<>(first, mapper.apply(second), third, fourth);
+    }
+
+    @Contract("_ -> new")
+    public <E> @NotNull Quad<A, B, E, D> mapThird(@NotNull Function<C, E> mapper) {
+        return new Quad<>(first, second, mapper.apply(third), fourth);
+    }
+
+    @Contract("_ -> new")
+    public <E> @NotNull Quad<A, B, C, E> mapFourth(@NotNull Function<D, E> mapper) {
+        return new Quad<>(first, second, third, mapper.apply(fourth));
+    }
+
+    @Contract("_, _, _, _ -> new")
+    public <E, F, G, H> @NotNull Quad<E, F, G, H> map(@NotNull Function<A, E> firstMapper,
+                                                @NotNull Function<B, F> secondMapper,
+                                                @NotNull Function<C, G> thirdMapper,
+                                                @NotNull Function<D, H> fourthMapper) {
+        return new Quad<>(
+                firstMapper.apply(first),
+                secondMapper.apply(second),
+                thirdMapper.apply(third),
+                fourthMapper.apply(fourth)
+        );
+    }
+
+    public <T> T reduce(@NotNull QuadFunction<A, B, C, D, T> reduceFunction) {
+        return reduceFunction.apply(first, second, third, fourth);
+    }
+
+    public void sendTo(@NotNull QuadConsumer<? super A, ? super B, ? super C, ? super D> action) {
+        action.accept(first, second, third, fourth);
+    }
+
+    @Contract(" -> new")
+    public @NotNull Trio<B, C, D> removeFirst() {
+        return new Trio<>(second, third, fourth);
+    }
+
+    @Contract(" -> new")
+    public @NotNull Trio<A, C, D> removeSecond() {
+        return new Trio<>(first, third, fourth);
+    }
+
+    @Contract(" -> new")
+    public @NotNull Trio<A, B, D> removeThird() {
+        return new Trio<>(first, second, fourth);
+    }
+
+    @Contract(" -> new")
+    public @NotNull Trio<A, B, C> removeFourth() {
+        return new Trio<>(first, second, third);
+    }
+
+    @Contract("_, _, _, _ -> new")
+    public static <T, U, V, X> @NotNull Quad<T, U, V, X> of(T first, U second, V third, X fourth) {
+        return new Quad<>(first, second, third, fourth);
+    }
+}
