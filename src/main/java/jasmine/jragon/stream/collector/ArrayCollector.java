b18618ffd81fc6364d344cc4b5106cdc72df4271
Mr. J <mjohnsonsdev@outlook.com>
2025-07-06 19:48:45 -0500

    Fixing the ArrayCollector

--- src/main/java/jasmine/jragon/stream/collector/ArrayCollector.java
+++ src/main/java/jasmine/jragon/stream/collector/ArrayCollector.java
@@ -1,18 +1,23 @@
 package jasmine.jragon.stream.collector;
 
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
-@NoArgsConstructor(staticName = "toArray")
+@RequiredArgsConstructor(staticName = "toArray")
 public final class ArrayCollector<E> extends ListAccumulator<E, E[]> {
+    private final Class<E> componentType;
+
     @Contract(pure = true)
     @SuppressWarnings("unchecked")
     @Override
     public @NotNull Function<List<E>, E[]> finisher() {
-        return l -> (E[]) l.toArray();
+        var conversion = (E[]) Array.newInstance(componentType, 0);
+        return l -> (E[]) Arrays.copyOf(l.toArray(), l.size(), conversion.getClass());
     }
 }
